537a82a892416d378b3b1cbf9d162e412d67e96a
BrendanKelly331 <36211292+BrendanKelly331@users.noreply.github.com>
2018-04-10 14:54:07 -0400

    Lab09 - MyMath class creation

---
+++ Lab09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath result1 = new MyMath();
+
+            result1.Multiply(2, 3);
+            Console.WriteLine("The result is: " + result1.getResult());
+
+            result1.Divide(2, 3);
+            Console.WriteLine("The result is: " + result1.getResult());
+
+            result1.Add(2, 3);
+            Console.WriteLine("The result is: " + result1.getResult());
+
+            result1.Subtract(2, 3);
+            Console.WriteLine("The result is: " + result1.getResult());
+        }
+    }
+}
